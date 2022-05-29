1d19678e58cca749e1ec8d192eb997d230e8ece7
icaro.campos <icaro.campos@deal.com.br>
2022-05-29 00:00:14 -0300

    Add Input, Select and submit

---
+++ src/components/form/SubmitButton.js
@@ -0,0 +1,13 @@
+import styles from './SubmitButton.module.css';
+
+function Select({ text }){
+    return (
+        <div>
+            <button className={styles.btn}>
+                {text}
+            </button>
+        </div>
+    );
+}
+
+export default Select;
