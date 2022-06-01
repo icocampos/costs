8412a664310079489afcb1d264204c8e55c2f03b
icaro.campos <icaro.campos@deal.com.br>
2022-06-01 14:05:16 -0300

    edit project

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -14,6 +14,7 @@ function Project() {
     
     const [project, setProject] = useState([]);
     const [showProjectForm, setShowProjectForm] = useState(false);
+    const [showServiceForm, setShowServiceForm] = useState(false);
     const [message, setMessage] = useState();
     const [type, setType] = useState();
 
@@ -34,7 +35,8 @@ function Project() {
     },[id])
 
     function editPost(project) {
-        //budget validation
+        setMessage('')
+        
         if(project.budget < project.cost){
             setMessage('O orçamento não pode ser menor que o custo total do projeto');
             setType('error');
@@ -60,6 +62,10 @@ function Project() {
 
     function toggleProjectForm() {
         setShowProjectForm(!showProjectForm);
+    }
+
+    function toggleServiceForm() {
+        setShowServiceForm(!showServiceForm);
     }
 
     return (
@@ -95,6 +101,19 @@ function Project() {
                         </div>
                     )}
                 </div>
+                <div className={styles.service_form_container}>
+                    <h2>Adicione um serviço:</h2>
+                    <button className={styles.btn} onClick={toggleServiceForm}>
+                        {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                    </button>
+                    <div className={styles.project_info}>
+                        {showServiceForm && <div>formulário do serviço</div>}
+                    </div>
+                </div>
+                <h2>Serviços</h2>
+                <Container customClass="start"> 
+                        <p>Itens de serviços</p>
+                </Container>
             </Container>
         </div> 
         ) : (
